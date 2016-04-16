7863eb63edb89c722197ece477022160850c6f81
letys <kontakt@letys.pl>
2016-04-16 13:05:30 +0200

    Wersja gotowa na GitHub

---
+++ WebApiBackboneJs/gulpfile.js
@@ -0,0 +1,23 @@
+﻿var gulp = require('gulp');
+
+gulp.task('copy_backbone', function () {
+    gulp.src('./lib/backbone/backbone-min.js')
+   .pipe(gulp.dest('./Scripts'));
+});
+
+gulp.task('copy_jQuery', function () {
+    gulp.src('./lib/jquery/dist/jquery.min.js')
+   .pipe(gulp.dest('./Scripts'));
+});
+
+gulp.task('copy_json2', function () {
+    gulp.src('./lib/json2/*.js')
+   .pipe(gulp.dest('./Scripts'));
+});
+
+gulp.task('copy_underscore', function () {
+    gulp.src('./lib/underscore/underscore-min.js')
+   .pipe(gulp.dest('./Scripts'));
+});
+
+gulp.task('default', ['copy_backbone', 'copy_jQuery', 'copy_json2', 'copy_underscore']);
